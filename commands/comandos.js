a294c15042d800f2394c4762c2a483107e4111c8
JackHammer1234 <jack.arts1231@gmail.com>
2025-06-26 18:15:03 -0600

    Update comandos.js

--- commands/comandos.js
+++ commands/comandos.js
@@ -1,10 +1,23 @@
+const { EmbedBuilder } = require("discord.js");
+
 module.exports = {
   name: "comandos",
   description: "Lista todos los comandos disponibles del bot.",
   execute(message) {
     const comandos = message.client.commands;
-    const nombres = comandos.map(cmd => cmd.name).join(", ");
+    const nombres = comandos.map(cmd => `\`${cmd.name}\``).join(", ");
 
-    message.channel.send(`**Comandos disponibles:**\n${nombres}`);
+    const embed = new EmbedBuilder()
+      .setTitle("`COMANDOS DISPONIBLES`") // ENCABEZADO CON FORMATO
+      .setDescription(
+        "--------------------------------------------------------------------------------------\n" + // SEPARADOR
+        "Lista de comandos que puedes usar:\n\n" +
+        "**Comandos disponibles:**\n" +
+        `${nombres}`
+      )
+      .setColor(0xf2b4ff);
+
+    message.channel.send({ embeds: [embed] });
   },
 };
+
